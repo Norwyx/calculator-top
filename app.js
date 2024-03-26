47a2188c62c3eba3b02be5ecf034e24d6c739c18
Manuel Salazar <mannisuso@gmail.com>
2024-03-26 11:48:41 -0500

    Create operate function and helper functions add, subtract, multiply, and divide

--- app.js
+++ app.js
@@ -11,3 +11,36 @@ let firstOperand = ''
 let secondOperand = ''
 let currentOperation = null
 
+function add (a, b) {
+    return a + b
+}
+
+function subtract (a, b) {
+    return a - b
+}
+
+function multiply (a, b) {
+    return a * b
+}
+
+function divide (a, b) {
+    return a / b
+}
+
+function operate (a, b, operator) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case '+':
+            return add(a, b)
+        case '-':
+            return subtract(a, b)
+        case '×':
+            return multiply(a, b)
+        case '÷':
+            if (b === 0) return null
+            else return divide(a, b)
+        default:
+            return null
+    }
+}
